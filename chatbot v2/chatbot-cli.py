26fa210626b499d44015a06c63a12a4014412490
Matthias ZWack <huegel.william@web.de>
2021-06-02 14:20:13 +0200

    chatbot-cli

---
+++ chatbot v2/chatbot-cli.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+from chatbotv2 import Chatbot
+
+def main():
+    # Listen
+    zufallsantworten = ["Oh wirklich...",
+                        "Interessant...",
+                        "Das kann man so sehen",
+                        "Ich verstehe"]
+    reaktionen = {"hallo": "aber hallo",
+                      "geht": "was verstehst du darunter?",
+                      "schmeckt": "ich habe keinen Geschmackssinn."}
+
+    # Ausgabe Begrüßung
+    print("Willkommen beim Chatbot v2")
+    print("Zum beenden geben Sie 'bye' ein...")
+    print("Worüber wollen Sie sprechen?")
+    print("")
+
+    # Chatbot-Objekt
+    bot = Chatbot(reaktionen, zufallsantworten)
+
+    # Logik
+    nutzereingabe = ""
+    while nutzereingabe !="bye":
+        nutzereingabe = ""
+        while nutzereingabe =="":
+                nutzereingabe = input("Ihre Frage oder Antwort: ")
+        if nutzereingabe == "bye":
+            break
+        bot.set_Message(nutzereingabe)
+        print(bot.get_Response())
+
+    # Ausgabe Verabschiedung
+    print("Bis zum nächsten Mal.")
+
+
+main ()
